d78f50514838282851fa82adc5b1fe9f6ffe4be8
unknown <fhim50>
2012-10-29 11:30:58 +0000

    working on home_base

--- register/views.py
+++ register/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 
 def home(request):
-	return render_to_response("home/base.html",{})
+	return render_to_response("home/home_base.html",{})
+	#return render_to_response("home/not_base.html",{})
